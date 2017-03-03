dae38be8f3a252507bf5e832bb84087425d33f9d
iulia <iuliamarialungu@yahoo.com>
2017-03-03 17:44:26 +0200

    added missing state value

--- components/Machine.js
+++ components/Machine.js
@@ -23,7 +23,7 @@ export default class Machine extends React.Component{
 		this.get = this.get.bind(this);
 		this.pay = this.pay.bind(this);
 		this.deliver = debounce(this.deliver,1500).bind(this);
-		this.reset = debounce(this.reset,2500).bind(this);
+		this.reset = debounce(this.reset,2000).bind(this);
 	}
 	getAll(){
 		this.setState({ busy : true });
@@ -38,11 +38,10 @@ export default class Machine extends React.Component{
 	}
     get(value) {
 	    console.log('got value',value);
-	    this.state.paid = false;
-	    this.productType = value;
+	    this.setState({ paid : false, productType : value});
         service.product(value)
 	        .then((product)=> {
-	        	console.log('product',product);
+	        	//console.log('product',product);
 	        	this.setState({ display: product.price });
 	        }).catch((e)=> console.log(e));
     }
@@ -50,9 +49,9 @@ export default class Machine extends React.Component{
     	this.getAll();
     }
     pay(){
-    	service.updateStock(this.productType)
+    	service.updateStock(this.state.productType)
 		    .then((res) => {
-		    	console.log('Stock updated', res);
+		    	//console.log('Stock updated', res);
 			    this.setState({ products: res, display: '0.00', paid: true, busy: true});
 			    this.deliver();
         }).catch((e)=>console.log(e));
@@ -60,12 +59,10 @@ export default class Machine extends React.Component{
     }
     deliver(){
     	this.setState({paid: true, busy: false});
-	    console.log('deliver paid', this.state.paid);
-	    this.productType = null; // reset machine;
+	    this.state.productType = null; // reset machine;
 	    this.reset();
     }
     reset(){
-	    console.log('reset paid', this.state.paid);
 	    this.setState({paid: false});
     }
 	render() {
@@ -85,8 +82,8 @@ export default class Machine extends React.Component{
 				    </div>
 			    </div>
 				<div className="panel-footer">
-					{!this.productType && !this.state.busy &&<p className="status">Select Product</p>}
-					{this.productType && !this.state.paid && <button className="btn btn-default" onClick={this.pay}>Pay amount</button>}
+					{!this.state.productType && !this.state.busy && <p className="status">Select Product</p>}
+					{this.state.productType && !this.state.paid && <button className="btn btn-default" onClick={this.pay}>Pay amount</button>}
 					{this.state.busy && <p className="status"><span>Delivering...</span><img src="../ajax-loader.gif" className="loader"/></p>}
 					{this.state.paid && !this.state.busy && <p className="status">Enjoy!</p>}
 				</div>
